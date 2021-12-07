924fc5651c0af3721550dbb13bb61c56a876bd64
sderbidge <55034668+sderbidge@users.noreply.github.com>
2021-12-07 15:46:18 -0700

    Changed greedy to farthest dist

--- TSPSolver.py
+++ TSPSolver.py
@@ -4,8 +4,8 @@ from which_pyqt import PYQT_VER
 
 if PYQT_VER == 'PYQT5':
     from PyQt5.QtCore import QLineF, QPointF
-elif PYQT_VER == 'PYQT4':
-    from PyQt4.QtCore import QLineF, QPointF
+# elif PYQT_VER == 'PYQT4':
+#     from PyQt4.QtCore import QLineF, QPointF
 else:
     raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
 
@@ -65,105 +65,87 @@ class TSPSolver:
         return results
 
     ''' <summary>
-		This is the entry point for the greedy solver, which you must implement for 
-		the group project (but it is probably a good idea to just do it for the branch-and
-		bound project as a way to get your feet wet).  Note this could be used to find your
-		initial BSSF.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found, the best
-		solution found, and three null values for fields not used for this 
-		algorithm</returns> 
-	'''
-
-    class Node:
-        def __init__(self, index, degree):
-            self.cityIndex = index
-            self.degree = degree
-
-        def updateDegree(self, newDegree):
-            degree = newDegree
-
-    class EdgeVal:
-        def __init__(self, node1, node2, cost):
-            self.node1 = node1
-            self.node2 = node2
-            self.cost = cost
+        This is the entry point for the greedy solver, which you must implement for 
+        the group project (but it is probably a good idea to just do it for the branch-and
+        bound project as a way to get your feet wet).  Note this could be used to find your
+        initial BSSF.
+        </summary>
+        <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+        time spent to find best solution, total number of solutions found, the best
+        solution found, and three null values for fields not used for this 
+        algorithm</returns> 
+        '''
 
     def greedy(self, time_allowance=60.0):
-        results = {}
         cities = self._scenario.getCities()
-        ncities = len(cities)
+        foundTour = False
+        count = 0
+        bssf = None
+        results = {}
         start_time = time.time()
-        sortedEdges = []
-        for i in range(ncities):
-            for j in range(i + 1, ncities):
-                cost = cities[i].costTo(cities[j])
-                if cost == math.inf:
-                    continue
-                node1 = self.Node(i, 0)
-                node2 = self.Node(j, 0)
-                newEdge = self.EdgeVal(node1, node2, cost)
-                sortedEdges.append(newEdge)
-        sortedEdges.sort(key=lambda x: x.cost)
-        edgeRoute = []
-        visited = set()
-        for edgeIndex in range(len(sortedEdges)):
-            if len(edgeRoute) == ncities:
-                break
-
-            if (not (sortedEdges[edgeIndex].node2.cityIndex in visited)) or (
-                    sortedEdges[edgeIndex].node1.degree < 2 and sortedEdges[edgeIndex].node2.degree < 2 and len(
-                    edgeRoute) == ncities - 1):
-                self.updateDegree(sortedEdges, sortedEdges[edgeIndex].node1.cityIndex)
-                self.updateDegree(sortedEdges, sortedEdges[edgeIndex].node2.cityIndex)
-                visited.add(sortedEdges[edgeIndex].node2.cityIndex)
-                edgeRoute.append(sortedEdges[edgeIndex])
         route = []
-        for i in range(len(edgeRoute) - 1, -1, -1):
-            if i == 0:
-                route.append(cities[edgeRoute[i].node1.cityIndex])
-                route.append(cities[edgeRoute[i].node2.cityIndex])
-            else:
-                route.append(cities[edgeRoute[i].node2.cityIndex])
+        while not foundTour and time.time() - start_time < time_allowance:
+            visited = set()
+            current_city = cities[count]
+            route.clear()
+            route.append(cities[count])
+            while len(visited) < len(cities):
+                visited.add(current_city)
+                max_city = None
+                max_cost = 0
+                for testCity in cities:
+                    if testCity not in visited:
+                        if max_cost < current_city.costTo(testCity) < math.inf:
+                            max_cost = current_city.costTo(testCity)
+                            max_city = testCity
+
+                if max_city is None:
+                    break
+
+                route.append(max_city)
+                current_city = max_city
+
+            count += 1
+            route_cost = TSPSolution(route).cost
+            if len(visited) == len(cities) and route_cost < math.inf:
+                if bssf is None:
+                    bssf = TSPSolution(route)
+                elif route_cost < bssf.cost:
+                    bssf = TSPSolution(route)
+
+            if count == len(cities):
+                foundTour = True
+
         end_time = time.time()
-        bssf = TSPSolution(route)
         results['cost'] = bssf.cost
         results['time'] = end_time - start_time
-        results['count'] = 1
+        results['count'] = count
         results['soln'] = bssf
-        results['max'] = None
+        results['max'] = 0
         results['total'] = None
         results['pruned'] = None
         return results
 
-    def updateDegree(self, list, cityIndex):
-        for i in range(len(list)):
-            if list[i].node1.cityIndex == cityIndex:
-                list[i].node1.degree += 1
-            elif list[i].node2.cityIndex == cityIndex:
-                list[i].node2.degree += 1
-
     ''' <summary>
-		This is the entry point for the branch-and-bound algorithm that you will implement
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number solutions found during search (does
-		not include the initial BSSF), the best solution found, and three more ints: 
-		max queue size, total number of states created, and number of pruned states.</returns> 
-	'''
+    This is the entry point for the branch-and-bound algorithm that you will implement
+    </summary>
+    <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+    time spent to find best solution, total number solutions found during search (does
+    not include the initial BSSF), the best solution found, and three more ints: 
+    max queue size, total number of states created, and number of pruned states.</returns> 
+    '''
 
     def branchAndBound(self, time_allowance=60.0):
         pass
 
     ''' <summary>
-		This is the entry point for the algorithm you'll write for your group project.
-		</summary>
-		<returns>results dictionary for GUI that contains three ints: cost of best solution, 
-		time spent to find best solution, total number of solutions found during search, the 
-		best solution found.  You may use the other three field however you like.
-		algorithm</returns> 
-	'''
+    This is the entry point for the algorithm you'll write for your group project.
+    </summary>
+    <returns>results dictionary for GUI that contains three ints: cost of best solution, 
+    time spent to find best solution, total number of solutions found during search, the 
+    best solution found.  You may use the other three field however you like.
+    algorithm</returns> 
+    '''
 
     def fancy(self, time_allowance=60.0):
         cities = self._scenario.getCities()
@@ -206,9 +188,9 @@ class TSPSolver:
                 foundTour = True
 
         end_time = time.time()
-        return self.createResults(bssf, start_time, end_time, count, foundTour)
+        return self.createResults(bssf, start_time, end_time, count, foundTour, 0)
 
     @staticmethod
-    def createResults(bssf, start_time, end_time, count, foundTour, max_queue_size=None, total=None, pruned=None):
+    def createResults(bssf, start_time, end_time, count, foundTour, max_queue_size, total=None, pruned=None):
         return {'cost': bssf.cost if foundTour else math.inf, 'time': end_time - start_time, 'count': count,
                 'soln': bssf, 'max': max_queue_size, 'total': total, 'pruned': pruned}
