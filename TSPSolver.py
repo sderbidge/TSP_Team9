5c984f6c4981d7b825966130394e23f07e39de14
sderbidge <55034668+sderbidge@users.noreply.github.com>
2021-12-07 13:30:21 -0700

    Testing greedy algorithm

--- TSPSolver.py
+++ TSPSolver.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
 from which_pyqt import PYQT_VER
+
 if PYQT_VER == 'PYQT5':
 	from PyQt5.QtCore import QLineF, QPointF
 elif PYQT_VER == 'PYQT4':
 	from PyQt4.QtCore import QLineF, QPointF
 else:
 	raise Exception('Unsupported Version of PyQt: {}'.format(PYQT_VER))
-
-
-
 
 import time
 import numpy as np
@@ -18,14 +16,12 @@ import heapq
 import itertools
 
 
-
 class TSPSolver:
-	def __init__( self, gui_view ):
+	def __init__(self, gui_view):
 		self._scenario = None
 
-	def setupWithScenario( self, scenario ):
+	def setupWithScenario(self, scenario):
 		self._scenario = scenario
-
 
 	''' <summary>
 		This is the entry point for the default solver
@@ -37,8 +33,8 @@ class TSPSolver:
 		solution found, and three null values for fields not used for this 
 		algorithm</returns> 
 	'''
-	
-	def defaultRandomTour( self, time_allowance=60.0 ):
+
+	def defaultRandomTour(self, time_allowance=60.0):
 		results = {}
 		cities = self._scenario.getCities()
 		ncities = len(cities)
@@ -46,13 +42,13 @@ class TSPSolver:
 		count = 0
 		bssf = None
 		start_time = time.time()
-		while not foundTour and time.time()-start_time < time_allowance:
+		while not foundTour and time.time() - start_time < time_allowance:
 			# create a random permutation
-			perm = np.random.permutation( ncities )
+			perm = np.random.permutation(ncities)
 			route = []
 			# Now build the route using the random permutation
-			for i in range( ncities ):
-				route.append( cities[ perm[i] ] )
+			for i in range(ncities):
+				route.append(cities[perm[i]])
 			bssf = TSPSolution(route)
 			count += 1
 			if bssf.cost < np.inf:
@@ -67,7 +63,6 @@ class TSPSolver:
 		results['total'] = None
 		results['pruned'] = None
 		return results
-
 
 	''' <summary>
 		This is the entry point for the greedy solver, which you must implement for 
@@ -95,15 +90,10 @@ class TSPSolver:
 			self.node2 = node2
 			self.cost = cost
 
-
-
-	def greedy( self,time_allowance=60.0 ):
+	def greedy(self, time_allowance=60.0):
 		results = {}
 		cities = self._scenario.getCities()
 		ncities = len(cities)
-		foundTour = False
-		count = 0
-		bssf = None
 		start_time = time.time()
 		sortedEdges = []
 		for i in range(ncities):
@@ -117,16 +107,41 @@ class TSPSolver:
 				sortedEdges.append(newEdge)
 		sortedEdges.sort(key=lambda x: x.cost)
 		edgeRoute = []
+		visited = set()
 		for edgeIndex in range(len(sortedEdges)):
 			if len(edgeRoute) == ncities:
 				break
-			sortedEdges[edgeIndex].node1.degree += 1
-			sortedEdges[edgeIndex].node2.degree += 1
-			if sortedEdges[edgeIndex].node1.degree <= 2 and sortedEdges[edgeIndex].node2.degree <= 2:
+
+			if (not (sortedEdges[edgeIndex].node2.cityIndex in visited)) or (sortedEdges[edgeIndex].node1.degree < 2 and sortedEdges[edgeIndex].node2.degree < 2 and len(edgeRoute) == ncities - 1):
+				self.updateDegree(sortedEdges, sortedEdges[edgeIndex].node1.cityIndex)
+				self.updateDegree(sortedEdges, sortedEdges[edgeIndex].node2.cityIndex)
+				visited.add(sortedEdges[edgeIndex].node2.cityIndex)
 				edgeRoute.append(sortedEdges[edgeIndex])
+		route = []
+		for i in range(len(edgeRoute) - 1, -1, -1):
+			if i == 0:
+				route.append(cities[edgeRoute[i].node1.cityIndex])
+				# route.append(cities[edgeRoute[i].node2.cityIndex])
+			else:
+				route.append(cities[edgeRoute[i].node2.cityIndex])
+		end_time = time.time()
+		bssf = TSPSolution(route)
+		results['cost'] = bssf.cost
+		results['time'] = end_time - start_time
+		results['count'] = 1
+		results['soln'] = bssf
+		results['max'] = None
+		results['total'] = None
+		results['pruned'] = None
 		return results
 
-	
+	def updateDegree(self, list, cityIndex):
+		for i in range(len(list)):
+			if list[i].node1.cityIndex == cityIndex:
+				list[i].node1.degree += 1
+			elif list[i].node2.cityIndex == cityIndex:
+				list[i].node2.degree += 1
+
 	''' <summary>
 		This is the entry point for the branch-and-bound algorithm that you will implement
 		</summary>
@@ -135,11 +150,9 @@ class TSPSolver:
 		not include the initial BSSF), the best solution found, and three more ints: 
 		max queue size, total number of states created, and number of pruned states.</returns> 
 	'''
-		
-	def branchAndBound( self, time_allowance=60.0 ):
+
+	def branchAndBound(self, time_allowance=60.0):
 		pass
-
-
 
 	''' <summary>
 		This is the entry point for the algorithm you'll write for your group project.
@@ -149,10 +162,6 @@ class TSPSolver:
 		best solution found.  You may use the other three field however you like.
 		algorithm</returns> 
 	'''
-		
-	def fancy( self,time_allowance=60.0 ):
+
+	def fancy(self, time_allowance=60.0):
 		pass
-		
-
-
-
